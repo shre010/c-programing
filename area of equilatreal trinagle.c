e0f2b20e63697156eda8804b96495b95b5234d8d
shre010 <130633330+shre010@users.noreply.github.com>
2024-03-11 13:19:51 +0530

    Add files via upload

---
+++ area of equilatreal trinagle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+
+  float side, area, temp;
+
+
+  printf("Enter the Side of the triangle: ");
+  scanf("%f",&side);
+
+
+  area = temp * side * side ;
+  printf("Area of Equilateral Triangle is: %f",area);
+
+  return 0;
+}
